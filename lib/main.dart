1710e3ded800179e76a938a4cde7c36cf689b64a
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-10 20:21:36 -0300

    Feat - inclusão da ferramenta que utiliza do ThemeMode da homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,51 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todolist/screens/home_screen.dart';
+import 'package:todolist/services/theme_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  final themeService = ThemeService();
+  final themeMode = await themeService.getThemeMode();
+
+  runApp(MyApp(themeMode: themeMode));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  final ThemeMode themeMode;
+  const MyApp({super.key, required this.themeMode});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late ThemeMode _themeMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _themeMode = widget.themeMode;
+  }
+
+  void _changeThemeMode(ThemeMode themeMode) async {
+    final themeService = ThemeService();
+    await themeService.saveThemeMode(themeMode);
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+      home: HomeScreen(onThemeChanged: _changeThemeMode),
     );
   }
 }
