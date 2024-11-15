d64f7885e9e4f8af3f36b1925e9cbf7524cf3297
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-15 11:26:36 -0300

    Feat - Adicionando visualização das tasks locais

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'add_task_screen.dart'; // A tela de adicionar tarefa
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todolist/models/Task.dart';
+import 'add_task_screen.dart';
 import '../services/auth_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,24 +18,51 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final AuthService _authService = AuthService();
   User? _user;
+  late Box<Task> _taskBox;
+  late Future<List<Task>> _tasksFuture;
 
   @override
   void initState() {
     super.initState();
-    _checkUserStatus();
+    _tasksFuture = Future.value([]); 
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    await _openTaskBox();
+    await _checkUserStatus();
+  }
+
+  Future<void> _openTaskBox() async {
+    _taskBox = await Hive.openBox<Task>('tasks');
+    _tasksFuture = _loadTasks(); 
+    setState(() {});
   }
 
   Future<void> _checkUserStatus() async {
     _user = FirebaseAuth.instance.currentUser;
+    _tasksFuture = _loadTasks();
     setState(() {});
   }
 
+  Future<List<Task>> _loadTasks() async {
+    if (_user == null) {
+      return _taskBox.values.toList();
+    } else {
+      // Lógica de tarefas do Firestore será adicionada futuramente
+      print("Usuário logado. Tarefas do Firestore ainda não implementadas.");
+      return [];
+    }
+  }
+
+  // Função de login com Google
   Future<void> _loginWithGoogle() async {
     try {
       User? user = await _authService.signInWithGoogle();
       if (user != null) {
         setState(() {
           _user = user;
+          _tasksFuture = _loadTasks();
         });
         print('Usuário logado: ${user.displayName}');
       } else {
@@ -43,13 +73,179 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  // Função de logout
   Future<void> _logout() async {
     await _authService.signOut();
     setState(() {
       _user = null;
+      _tasksFuture = _loadTasks(); 
     });
     print('Usuário deslogado');
-    Navigator.pop(context); // Fecha o Dialog após o logout
+    Navigator.pop(context);
+  }
+
+  // Função de exibição do perfil do usuário
+  void _showUserProfile() {
+    showDialog(
+      context: context,
+      barrierDismissible: true, 
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context); 
+                    },
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: _user?.photoURL != null
+                      ? NetworkImage(_user!.photoURL!)
+                      : null,
+                  child: _user?.photoURL == null
+                      ? const Icon(Icons.person, size: 60)
+                      : null,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  _user?.displayName ?? 'Nome não disponível',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  _user?.email ?? 'Email não disponível',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 24),
+                const Divider(),
+                TextButton(
+                  onPressed: _logout,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.logout, color: Colors.black),
+                      SizedBox(width: 8),
+                      Text('Sair'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _onTaskAdded() {
+  setState(() {
+    _tasksFuture = _loadTasks(); 
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.brightness_6),
+            onPressed: _showThemeDialog,
+          ),
+        ],
+        title: GestureDetector(
+          onTap: _user == null ? _loginWithGoogle : _showUserProfile, 
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 16,
+                backgroundImage: _user?.photoURL != null
+                    ? NetworkImage(_user!.photoURL!)
+                    : null,
+                child: _user?.photoURL == null
+                    ? const Icon(Icons.person, size: 16)
+                    : null,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                _user == null ? 'Login' : _user!.displayName ?? 'Usuário',
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Task>>(
+          future: _tasksFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Erro: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('Nenhuma tarefa salva'));
+            }
+
+            final tasks = snapshot.data!; 
+
+            return ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+                return ListTile(
+                  title: Text(task.title),
+                  subtitle: Text(task.description),
+                  trailing: Icon(
+                    task.completed
+                        ? Icons.check_box
+                        : Icons.check_box_outline_blank,
+                    color: task.completed ? Colors.green : Colors.grey,
+                  ),
+                  onTap: () {
+                    // Aqui pode-se implementar a edição ou marcação de tarefa como concluída
+                  },
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTaskScreen(onTaskAdded: _onTaskAdded)),
+          );
+
+          if (result != null && result) {
+            setState(() {
+              _tasksFuture = _loadTasks();
+            });
+          }
+        },
+        child: const Icon(Icons.add),
+        tooltip: 'Adicionar Tarefa',
+      ),
+    );
   }
 
   void _showThemeDialog() {
@@ -86,127 +282,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         );
       },
-    );
-  }
-
-  void _showUserProfile() {
-    showDialog(
-      context: context,
-      barrierDismissible: true, // Permite fechar o diálogo ao clicar fora dele
-      builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: () {
-                      Navigator.pop(context); // Fecha o Dialog
-                    },
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: _user?.photoURL != null
-                      ? NetworkImage(_user!.photoURL!)
-                      : null,
-                  child: _user?.photoURL == null
-                      ? const Icon(Icons.person, size: 60)
-                      : null,
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  _user?.displayName ?? 'Nome não disponível',
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  _user?.email ?? 'Email não disponível',
-                  style: const TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 24),
-                const Divider(),
-                TextButton(
-                  onPressed: _logout, // Chama logout e fecha o Dialog
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Icon(Icons.logout, color: Colors.black),
-                      SizedBox(width: 8),
-                      Text('Sair'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.brightness_6),
-            onPressed: _showThemeDialog,
-          ),
-        ],
-        title: GestureDetector(
-          onTap: _user == null ? _loginWithGoogle : _showUserProfile, // Exibe o perfil do usuário
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 16,
-                backgroundImage: _user?.photoURL != null
-                    ? NetworkImage(_user!.photoURL!)
-                    : null,
-                child: _user?.photoURL == null
-                    ? const Icon(Icons.person, size: 16)
-                    : null,
-              ),
-              const SizedBox(width: 8),
-              Text(
-                _user == null ? 'Login' : _user!.displayName ?? 'Usuário',
-                style: const TextStyle(fontSize: 16),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Center(
-        child: _user == null
-            ? Container() // Corpo vazio se o usuário não estiver logado
-            : const SizedBox.shrink(), // Não exibe nada quando o usuário está logado
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navega para a tela de adicionar tarefa
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddTaskScreen()),
-          );
-        },
-        child: const Icon(Icons.add),
-        tooltip: 'Adicionar Tarefa',
-      ),
     );
   }
 }
