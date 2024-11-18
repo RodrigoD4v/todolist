b06e4deb6ee3b62869e941255632362da126447a
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-18 07:59:29 -0300

    Feat - Criando função para converter um objeto vindo do firestore para o tipo Task

--- lib/models/Task.dart
+++ lib/models/Task.dart
@@ -1,5 +1,6 @@
 import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 part 'Task.g.dart';
 
@@ -34,5 +35,18 @@ class Task {
   });
 
   factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+
+  factory Task.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Task(
+      id: doc.id,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      createdAt: (data['createdAt'] as Timestamp).toDate(),
+      updatedAt: (data['updatedAt'] as Timestamp).toDate(),
+      completed: data['completed'] ?? false,
+    );
+  }
+
   Map<String, dynamic> toJson() => _$TaskToJson(this);
 }
