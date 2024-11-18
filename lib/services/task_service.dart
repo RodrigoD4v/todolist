87432c5afa213971509944b0b87142af4008f08f
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-18 11:19:51 -0300

    Feat - Criando método responsável pela exclusão local de task e requisição para exclusão no firestore caso logado

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -12,12 +12,18 @@ class TaskService {
 
   TaskService(this.taskBox);
 
+    Future<void> syncTasksAfterLogin() async {
+    if (_auth.currentUser != null) {
+      await syncTasksWithApi();
+    }
+  }
+
   // Função para enviar as tarefas para a API
   Future<void> syncTasksWithApi() async {
-    var tasks = taskBox.values.toList(); // Pega todas as tarefas do Hive
+    var tasks = taskBox.values.toList();
     String? apiUrl = dotenv.env['API_URL'];
 
-     if (apiUrl == null || apiUrl.isEmpty) {
+    if (apiUrl == null || apiUrl.isEmpty) {
       return;
     }
 
@@ -28,8 +34,7 @@ class TaskService {
         try {
           // Envia a tarefa para a API com o UID do usuário logado
           final response = await http.post(
-            Uri.parse(
-                '$apiUrl/tasks/${user.uid}'),
+            Uri.parse('$apiUrl/tasks/${user.uid}'),
             headers: {
               'Content-Type': 'application/json',
             },
@@ -75,5 +80,43 @@ class TaskService {
 
     // Chama a função para sincronizar com a API
     await syncTasksWithApi();
+  }
+
+  // Função para excluir tarefa
+  Future<void> deleteTask(String taskId) async {
+    User? user = _auth.currentUser;
+
+    if (user != null) {
+      try {
+        String? apiUrl = dotenv.env['API_URL'];
+
+        if (apiUrl == null || apiUrl.isEmpty) {
+          return;
+        }
+
+        final response = await http.delete(
+          Uri.parse('$apiUrl/tasks/${user.uid}/$taskId'),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        );
+
+        if (response.statusCode == 200) {
+          print("Tarefa excluída com sucesso.");
+          
+          // Excluir tarefa localmente do Hive
+          final taskIndex = taskBox.values.toList().indexWhere((task) => task.id == taskId);
+          if (taskIndex != -1) {
+            await taskBox.deleteAt(taskIndex);
+          }
+        } else {
+          print("Erro ao excluir tarefa: ${response.body}");
+        }
+      } catch (error) {
+        print("Erro ao excluir tarefa: $error");
+      }
+    } else {
+      print("Usuário não está autenticado.");
+    }
   }
 }
